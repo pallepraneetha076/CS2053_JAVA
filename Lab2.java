da7a52c2fbc6398be3277fb1e463342401c4beb5
Palle Praneetha <pallep.bsc23@rvu.edu.in>
2025-10-26 13:02:04 +0530

    Implement Lab2 to display employee details from args

---
+++ Lab2.java
@@ -0,0 +1,26 @@
+public class Lab2 {
+    public static void main(String args[]) {
+        System.out.println("Total number of arguments: " + args.length);
+
+        if (args.length == 4) {
+            System.out.println("--- Provided Arguments ---");
+            for (int i = 0; i < args.length; i++) {
+                System.out.println("Argument " + i + ": " + args[i]);
+            }
+
+            // Assigning command-line arguments to employee details
+            String empId = args[0];
+            String name = args[1];
+            String department = args[2];
+            String email = args[3];
+
+            System.out.println("\n--- Employee Details ---");
+            System.out.println("Employee ID: " + empId);
+            System.out.println("Employee Name: " + name);
+            System.out.println("Department: " + department);
+            System.out.println("Email ID: " + email);
+        } else {
+            System.out.println("Please provide exactly 4 arguments for employee details.");
+        }
+    }
+}
