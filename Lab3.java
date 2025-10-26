31bd167369a73dd29db60525d9f63282cae18ec4
Palle Praneetha <pallep.bsc23@rvu.edu.in>
2025-10-26 13:04:38 +0530

    Add Vehicle, Car, and ElectricCar classes

---
+++ Lab3.java
@@ -0,0 +1,53 @@
+class Vehicle {
+    String brand;
+    int speed;
+
+    Vehicle(String brand, int speed) {
+        this.brand = brand;
+        this.speed = speed;
+        System.out.println("Vehicle constructor executed");
+    }
+
+    void displayInfo() {
+        System.out.println("Brand: " + brand);
+        System.out.println("Speed: " + speed + " km/h");
+    }
+}
+
+class Car extends Vehicle {
+    int fuelCapacity;
+
+    Car(String brand, int speed, int fuelCapacity) {
+        super(brand, speed);   
+        this.fuelCapacity = fuelCapacity;
+        System.out.println("Car constructor executed");
+    }
+
+    void displayInfo() {
+        super.displayInfo();   
+        System.out.println("Fuel Tank Capacity: " + fuelCapacity + " liters");
+    }
+}
+
+class ElectricCar extends Car {
+    int batteryCapacity;
+
+    ElectricCar(String brand, int speed, int fuelCapacity, int batteryCapacity) {
+        super(brand, speed, fuelCapacity);   
+        this.batteryCapacity = batteryCapacity;
+        System.out.println("ElectricCar constructor executed");
+    }
+
+    void displayInfo() {
+        super.displayInfo();   
+        System.out.println("Battery Capacity: " + batteryCapacity + " kWh");
+    }
+}
+
+public class Lab3 {
+    public static void main(String[] args) {
+        ElectricCar tesla = new ElectricCar("Tesla", 200, 0, 100);
+        System.out.println("\n--- Electric Vehicle Details ---");
+        tesla.displayInfo();
+    }
+}
